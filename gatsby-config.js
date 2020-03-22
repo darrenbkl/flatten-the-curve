173bd73c8f56e40f03119230608480e4ae82934d
Darren Boo <darrenbkl@gmail.com>
2020-03-22 22:56:48 +0800

    Extract google cred envvar

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,18 @@ module.exports = {
       options: {
         spreadsheetId: "1G-cp5XamvC38FbCM4u33JTZFCWkFtP_dcw51CeQKg68",
         worksheetTitle: "wfh",
-        credentials: require("./google-credentials.json"),
+        credentials: {
+          "type": "service_account",
+          "project_id": process.env.GATSBY_GOOGLE_PROJECT_ID,
+          "private_key_id": process.env.GATSBY_GOOGLE_PRIVATE_KEY_ID,
+          "private_key": process.env.GATSBY_GOOGLE_PRIVATE_KEY,
+          "client_email": process.env.GATSBY_GOOGLE_CLIENT_EMAIL,
+          "client_id": process.env.GATSBY_GOOGLE_CLIENT_ID,
+          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+          "token_uri": "https://oauth2.googleapis.com/token",
+          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+          "client_x509_cert_url": process.env.GATSBY_GOOGLE_CLIENT_X509_CERT_URL
+        }      
       },
     },
   ],
